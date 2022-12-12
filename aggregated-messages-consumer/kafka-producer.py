2932e5f7da9588b5338ef9580315cfae7d68710f
charlyjazz <carlosjazzc1@gmail.com>
2022-12-11 22:42:52 -0300

    Add message id to producer

--- aggregated-messages-consumer/kafka-producer.py
+++ aggregated-messages-consumer/kafka-producer.py
@@ -14,6 +14,7 @@ MESSAGES = [
     "to_user": "other_user",
     "body": "Hello other user",
     "chat_id": 2,
+    "message_id": 1,
     "time": str(datetime.today())
   },
   {
@@ -21,6 +22,7 @@ MESSAGES = [
     "to_user": "evil",
     "body": "Hello bro",
     "chat_id": 1,
+    "message_id": 2,
     "time": str(datetime.today())
   },
   {
@@ -28,6 +30,7 @@ MESSAGES = [
     "to_user": "charlyjazz",
     "body": "What?",
     "chat_id": 1,
+    "message_id": 3,
     "time": str(datetime.today())
   },
   {
@@ -35,6 +38,7 @@ MESSAGES = [
     "to_user": "evil",
     "body": "Pepe le pu",
     "chat_id": 1,
+    "message_id": 4,
     "time": str(datetime.today())
   },
   {
@@ -42,6 +46,7 @@ MESSAGES = [
     "to_user": "charlyjazz",
     "body": "Ohh I see",
     "chat_id": 1,
+    "message_id": 5,
     "time": str(datetime.today())
   },
   {
@@ -49,6 +54,7 @@ MESSAGES = [
     "to_user": "evil",
     "body": "Bye bro",
     "chat_id": 1,
+    "message_id": 6,
     "time": str(datetime.today())
   }
 ]
