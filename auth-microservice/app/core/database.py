c6317c84e2ae73913c9a2eeb3f0ea19415e4e540
Carlos Azuaje <carlosjazzc1@gmail.com>
2023-10-02 12:01:26 -0300

    feat(docker): auth microsevrice
    * Create dockerfile for auth server
    
    * fix POSTGRES_PORT thanks haxor

--- auth-microservice/app/core/database.py
+++ auth-microservice/app/core/database.py
@@ -3,5 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
+print("Settings Variables: {}".format(settings))
+
 engine = create_async_engine(settings.POSTGRES_URI)
 SessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
