8cf9200f05b9cf208c7269e5a62a7df1259d0a19
charlyjazz <carlosjazzc1@gmail.com>
2023-11-15 12:12:02 -0300

    fix(log): consumer logger

--- aggregated-messages-consumer/run.py
+++ aggregated-messages-consumer/run.py
@@ -2,19 +2,40 @@ from kafka import KafkaClient, errors
 from kafka_consumer import KafkaMessageConsumer, BOOTSTRAP_SERVER_ADDRESS, CONTAINER_MODE
 
 import logging
-import time
+import json
 import os
+import sys
 
-logging.warning("CONTAINER_MODE = <{}>".format(CONTAINER_MODE))
+class JsonFormatter(logging.Formatter):
+    def format(self, record):
+        log_record = {
+            "name": record.name,
+            "level": record.levelname,
+            "message": record.getMessage(),
+            "timestamp": self.formatTime(record, self.datefmt),
+        }
 
-logging.warning("BOOTSTRAP_SERVER_ADDRESS = <{}>".format(BOOTSTRAP_SERVER_ADDRESS))
+        log_record['environment'] = 'development'
+        log_record['service'] = 'aggregated-messages-consumer'
+
+        if record.exc_info:
+            log_record['exception'] = self.formatException(record.exc_info)
+
+        return json.dumps(log_record)
 
 
 if CONTAINER_MODE == "1":
     logging.basicConfig(
         level=logging.INFO,
-        format='%(name)s - %(levelname)s - %(message)s'
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%dT%H:%M:%S',
+        handlers=[
+            logging.StreamHandler(sys.stdout),  # Ensures logs go to stdout which is what Docker expects
+        ]
     )
+
+    # Setting the formatter for the root logger to use the JsonFormatter
+    logging.getLogger().handlers[0].setFormatter(JsonFormatter())
 elif CONTAINER_MODE == "0":
     logging.basicConfig(
         level=logging.DEBUG,
@@ -23,6 +44,9 @@ elif CONTAINER_MODE == "0":
         format='%(name)s - %(levelname)s - %(message)s'
     )
 
+logging.info("CONTAINER_MODE = <{}>".format(CONTAINER_MODE))
+
+logging.info("BOOTSTRAP_SERVER_ADDRESS = <{}>".format(BOOTSTRAP_SERVER_ADDRESS))
 
 def check_kafka_availability(timeout_seconds=120):
     start_time = time.time()
