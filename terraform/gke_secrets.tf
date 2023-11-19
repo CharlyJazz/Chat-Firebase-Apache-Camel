b50ef996ed7ad25fbbd6cc484a7f5661dbc72517
charlyjazz <carlosjazzc1@gmail.com>
2023-11-18 22:48:49 -0300

    deploy front end

--- terraform/gke_secrets.tf
+++ terraform/gke_secrets.tf
@@ -8,12 +8,12 @@ resource "kubernetes_secret" "firebase_secret" {
   }
 }
 
-# resource "kubernetes_secret" "frontend_secrets" {
-#   metadata {
-#     name = "frontend-secrets"
-#   }
+resource "kubernetes_secret" "frontend_secrets" {
+  metadata {
+    name = "frontend-secrets"
+  }
 
-#   data = {
-#     for k, v in tomap({ for s in split("\n", trimspace(file("${path.module}/.env"))) : split("=", s)[0] => split("=", s)[1] }) : k => v
-#   }
-# }
+  data = {
+    "file.txt" = file("${path.module}/env.prod")
+  }
+}
