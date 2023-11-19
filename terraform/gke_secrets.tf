b515da584022cb117d565249226cb33841107fc1
charlyjazz <carlosjazzc1@gmail.com>
2023-11-18 23:52:20 -0300

    Add some comments

--- terraform/gke_secrets.tf
+++ terraform/gke_secrets.tf
@@ -1,3 +1,5 @@
+# kubectl create secret generic firebase-secret -from-file=service_account_key.json=./firestore_service_account.json
+# Needed by the consumer server
 resource "kubernetes_secret" "firebase_secret" {
   metadata {
     name = "firebase-secret"
@@ -8,4 +10,6 @@ resource "kubernetes_secret" "firebase_secret" {
   }
 }
 
-# I don't know how to do kubectl create secret generic frontend-secrets --from-env-file=.env
+# kubectl create secret generic frontend-secrets --from-env-file=.env
+# Needed by the front end
+# I don't know how to translate it to terraform...
