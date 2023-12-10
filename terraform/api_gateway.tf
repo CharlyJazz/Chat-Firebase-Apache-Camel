1ecb31cee87dabdb8115d90831a893c940eedeaf
charlyjazz <carlosjazzc1@gmail.com>
2023-12-10 03:42:41 -0300

    Try to implement native vpc cluster

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -1,16 +1,3 @@
-resource "google_compute_subnetwork" "managed_proxy_subnet" {
-  role          = "ACTIVE"
-  purpose       = "REGIONAL_MANAGED_PROXY"
-  name          = var.managed_proxy_subnet
-  region        = var.region
-  network       = google_compute_network.vpc.name
-  ip_cidr_range = "10.0.32.0/20"
-
-  depends_on = [google_compute_network.vpc]
-}
-
-# Verify your proxy-only subnet
-# gcloud compute networks subnets describe my-gke-proxy-subnet --region=us-central1
 
 data "kubectl_filename_list" "api_gateway_manifests" {
   pattern = "../api-gateway/*.yaml"
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,13 +1,40 @@
-# VPC
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/using_gke_with_terraform
+# The Gateway API is supported on VPC-native clusters only.
+
+#VPC
 resource "google_compute_network" "vpc" {
   name                    = "${var.project_id}-vpc"
-  auto_create_subnetworks = "false"
+  auto_create_subnetworks = false
 }
 
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
   name          = "${var.project_id}-subnet"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = var.region
+  network       = google_compute_network.subnet.id
+  secondary_ip_range {
+    range_name    = "services-range"
+    ip_cidr_range = "192.168.1.0/24"
+  }
+
+  secondary_ip_range {
+    range_name    = "pod-ranges"
+    ip_cidr_range = "192.168.64.0/22"
+  }
+}
+
+# Proxy Subnet
+resource "google_compute_subnetwork" "managed_proxy_subnet" {
+  role          = "ACTIVE"
+  purpose       = "REGIONAL_MANAGED_PROXY"
+  name          = var.managed_proxy_subnet
   region        = var.region
   network       = google_compute_network.vpc.name
-  ip_cidr_range = "10.10.0.0/24"
+  ip_cidr_range = "10.0.32.0/20"
+
+  depends_on = [google_compute_network.vpc]
 }
+
+# Verify your proxy-only subnet
+# gcloud compute networks subnets describe my-gke-proxy-subnet --region=us-central1
