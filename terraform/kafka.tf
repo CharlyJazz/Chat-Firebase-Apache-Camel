80fb43ba65fc451fa8310f41e6b92cd86b40148d
Carlos Azuaje <carlosjazzc1@gmail.com>
2023-12-27 20:39:08 -0300

    Update kafka.tf

--- terraform/kafka.tf
+++ terraform/kafka.tf
@@ -5,4 +5,11 @@ data "kubectl_filename_list" "manifests" {
 resource "kubectl_manifest" "kafka_and_zookeeper" {
   count     = length(data.kubectl_filename_list.manifests.matches)
   yaml_body = file(element(data.kubectl_filename_list.manifests.matches, count.index))
+
+  depends_on = [
+    google_container_cluster.primary,
+    google_compute_network.vpc,
+    google_compute_subnetwork.subnet,
+    google_compute_subnetwork.managed_proxy_subnet,
+  ]
 }
